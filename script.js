b87a143d4b15905dc363484aab57350ec3628f99
qwerty152 <kotm7175@gmail.com>
2024-12-09 12:01:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('home').style.display = 'none'; 
+
+document.getElementById('boarding__btn').addEventListener('click', function() {  
+	document.getElementById('boarding').style.display = 'none';  
+	document.getElementById('home').style.display = 'block';  
+});  
+
